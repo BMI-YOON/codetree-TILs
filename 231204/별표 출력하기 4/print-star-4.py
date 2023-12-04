15a2725aeeb89bdd71fcb0adc1506ff25f647c63
BMI-YOON <41634994+BMI-YOON@users.noreply.github.com>
2023-12-04 15:36:25 +0900

    Title: 별표 출력하기 4, Time: 136ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231204/별표 출력하기 4/print-star-4.py
@@ -0,0 +1,9 @@
+n = int(input())
+for i in range(n):
+    for _ in range(n-i):
+        print("*", end = ' ')
+    print()
+for i in range(n-2, -1, -1):
+    for _ in range(n-i):
+        print("*", end = ' ')
+    print()
