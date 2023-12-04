56d7a4b2d5a8182a9d9d707200d9112718a431d7
BMI-YOON <41634994+BMI-YOON@users.noreply.github.com>
2023-12-04 15:58:05 +0900

    Title: 문자열 한 칸씩 밀어내며 뒤집기, Time: 215ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 231204/문자열 한 칸씩 밀어내며 뒤집기/shift-reverse-string.py
@@ -0,0 +1,13 @@
+string, q = input().split()
+q = int(q)
+for _ in range(q):
+    num = int(input())
+    if num == 1:
+        string = string[1:] + string[0]
+        print(string)
+    elif num == 2:
+        string = string[-1] + string[:-1]
+        print(string)
+    else:
+        string = string[::-1]
+        print(string)
