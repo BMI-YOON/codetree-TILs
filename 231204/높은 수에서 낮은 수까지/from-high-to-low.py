49890495fdb3a21c03ba446928d17b3ad23dbf46
BMI-YOON <41634994+BMI-YOON@users.noreply.github.com>
2023-12-04 15:27:36 +0900

    Title: 높은 수에서 낮은 수까지, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231204/높은 수에서 낮은 수까지/from-high-to-low.py
@@ -0,0 +1,8 @@
+arr = input().split()
+a, b = int(arr[0]), int(arr[1])
+if a >= b:
+    for i in range(a, b-1, -1):
+        print(i, end = ' ')
+if a < b:
+    for i in range(b, a-1, -1):
+        print(i, end = ' ')
