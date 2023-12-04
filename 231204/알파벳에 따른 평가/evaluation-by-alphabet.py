600dba479ecd94379f88a6417b04094a737850e6
BMI-YOON <41634994+BMI-YOON@users.noreply.github.com>
2023-12-04 12:37:54 +0900

    Title: 알파벳에 따른 평가, Time: 105ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231204/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,13 @@
+Char = input()
+if Char == 'S':
+    print("Superior")
+elif Char == 'A':
+    print("Excellent")
+elif Char == 'B':
+    print("Good")
+elif Char == 'C':
+    print("Usually")
+elif Char == 'D':
+    print("Effort")
+else:
+    print("Failure")
