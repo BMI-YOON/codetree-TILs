b605de2f9e117ddb1b6a60d3c6cab90c557040c0
BMI-YOON <41634994+BMI-YOON@users.noreply.github.com>
2023-12-04 15:33:17 +0900

    Title: 출석 부르기 2, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231204/출석 부르기 2/calling-attendance-2.py
@@ -0,0 +1,13 @@
+while True:
+    num = int(input())
+    if num == 1:
+        print("John")
+    if num == 2:
+        print("Tom")
+    if num == 3:
+        print("Paul")
+    if num == 4:
+        print("Sam")
+    if num > 4:
+        print("Vacancy")
+        break
