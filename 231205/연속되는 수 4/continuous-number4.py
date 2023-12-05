d179e41f8c42c95a3330eeb189dff766a6e31b5f
BMI-YOON <41634994+BMI-YOON@users.noreply.github.com>
2023-12-05 15:23:16 +0900

    Title: 연속되는 수 4, Time: 216ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231205/연속되는 수 4/continuous-number4.py
@@ -0,0 +1,14 @@
+N = int(input())
+arr = [
+    int(input())
+    for _ in range(N)
+]
+
+ans = cnt = 0
+for i in range(N):
+    if i >= 1 and arr[i] > arr[i-1]:
+        cnt += 1
+    else:
+        cnt = 1
+    ans = max(ans, cnt)
+print(ans)
