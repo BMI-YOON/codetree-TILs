e33651f0a3e99a730bdc47eff8d8b0267095f1ce
BMI-YOON <41634994+BMI-YOON@users.noreply.github.com>
2023-12-05 14:59:34 +0900

    Title: 최소공배수 구하기, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231205/최소공배수 구하기/find-the-least-common-multiple.py
@@ -0,0 +1,9 @@
+def f(n, m):
+    max_val = 1
+    for i in range(1, min(n, m) + 1):
+        if n % i == 0 and m % i == 0:
+            max_val = i 
+    print(n * m // max_val)
+
+n, m = tuple(map(int, input().split()))
+f(n, m)
