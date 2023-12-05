b029ba3d91b6ccf69277edfd3e0b99d5115a582c
BMI-YOON <41634994+BMI-YOON@users.noreply.github.com>
2023-12-05 15:19:24 +0900

    Title: 2진수로 변환하기, Time: 123ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 231205/2진수로 변환하기/convert-to-binary.py
@@ -0,0 +1,10 @@
+n = int(input())
+digits = []
+while True:
+    if n < 2:
+        digits.append(n)
+        break
+    digits.append(n % 2)
+    n //= 2
+for elem in digits[::-1]:
+    print(elem, end = '')
