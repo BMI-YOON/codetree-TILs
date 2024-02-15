299df42af7074ebeabba3734fed51b44e510ecfa
BMI-YOON <41634994+BMI-YOON@users.noreply.github.com>
2024-02-16 08:49:02 +0900

    Title: 사다리 타기, Time: 941ms, Memory: 28MB, Status: Passed - CodeTree

---
+++ 240216/사다리 타기/ladder-game.py
@@ -0,0 +1,47 @@
+n, m = map(int, input().split())
+arr = [
+    tuple(map(int, input().split()))
+    for _ in range(m)
+]
+
+selected = list()
+minimum = m 
+
+def go(num):
+    global selected
+    cur = 15
+    while cur > 0:
+        if (num, cur) in selected:
+            num += 1
+        elif (num-1, cur) in selected:
+            num -= 1
+        cur -= 1
+    return num 
+
+def cal():
+    global n, selected 
+    candidate = list()
+    for i in range(1, n+1):
+        candidate.append(go(i))
+    return candidate 
+
+for i in range(m):
+    selected.append(arr[i])
+ans = cal()
+for i in range(m):
+    selected.pop()
+
+def simulate(k):
+    global n, m, arr, selected, minimum, ans
+    if k == m:
+        candidate = cal()
+        if candidate == ans:
+            minimum = min(minimum, len(selected))
+        return 
+    simulate(k+1)
+    selected.append((arr[k][0], arr[k][1]))
+    simulate(k+1)
+    selected.pop()
+
+simulate(0)
+print(minimum)
