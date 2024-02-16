9254aab5db3197809982ba5dbb67572660cafa39
BMI-YOON <41634994+BMI-YOON@users.noreply.github.com>
2024-02-16 13:12:48 +0900

    Title: 알파벳과 사칙연산, Time: 228ms, Memory: 26MB, Status: Passed - CodeTree

--- 240216/알파벳과 사칙연산/calculations-with-alphabet.py
+++ 240216/알파벳과 사칙연산/calculations-with-alphabet.py
@@ -1,32 +1,41 @@
 import sys
 
 question = list(input())
+alphabet = list()
+new_question = list()
 n = len(question)
 max_ans = -sys.maxsize
 
 def calculate():
-    global question, n
+    global new_question, n
 
-    ans = question[0]
+    ans = new_question[0]
     for i in range(1, n, 2):
-        if question[i] == '+':
-            ans += question[i+1]
-        elif question[i] == '-':
-            ans -= question[i+1]
+        if new_question[i] == '+':
+            ans += new_question[i+1]
+        elif new_question[i] == '-':
+            ans -= new_question[i+1]
         else:
-            ans *= question[i+1]
+            ans *= new_question[i+1]
     return ans 
 
 def simulate(cur_idx):
-    global question, n, max_ans 
+    global question, alphabet, new_question, n, max_ans 
 
-    if cur_idx == n+1:
+    if cur_idx == 6:
+        new_question = list()
+        for i in range(n):
+            if question[i] == '*' or question[i] == '+' or question[i] == '-':
+                new_question.append(question[i])
+            else:
+                new_question.append(alphabet[ord(question[i]) - 97])
         max_ans = max(max_ans, calculate())
         return
     
     for num in range(1, 5):
-        question[cur_idx] = num 
-        simulate(cur_idx+2)
+        alphabet.append(num) 
+        simulate(cur_idx+1)
+        alphabet.pop()
 
 simulate(0)
 print(max_ans)
