0242945eb3d3555541490576f13852f7c77bab43
BMI-YOON <41634994+BMI-YOON@users.noreply.github.com>
2024-02-21 11:17:51 +0900

    Title: 가능한 수열 중 최솟값 구하기, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240221/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.py
+++ 240221/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.py
@@ -5,20 +5,22 @@ def check():
     global n, selected 
     max_length = int(n/2)
     for length in range(1, max_length + 1):
-        for i in range(n-length*2+1):
+        for i in range(len(selected)-length*2+1):
             if selected[i:i+length] == selected[i+length:i+length+length]:
                 return False 
     return True
 
 def simulate(cur):
     global n, selected
-
     if cur == n:
         for elem in selected:
             print(elem, end = '')
         exit(0)
     for num in range(4, 7):
         selected.append(num)
+        print(selected)
+        print(num)
+        print(check())
         if check():
             simulate(cur+1)
         selected.pop()
