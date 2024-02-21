25439a044bcc0974a7772ac2598c3d6a01960a07
BMI-YOON <41634994+BMI-YOON@users.noreply.github.com>
2024-02-21 12:33:06 +0900

    Title: 최소 점프 횟수, Time: 143ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240221/최소 점프 횟수/min-num-of-jumps.py
@@ -0,0 +1,17 @@
+import sys
+
+n = int(input())
+arr = list(map(int, input().split()))
+ans = sys.maxsize
+
+def simulate(cur, cnt):
+    global n, arr, ans 
+    
+    if cur == n-1:
+        ans = min(ans, cnt)
+        return
+    for next in range(cur+1, cur+arr[cur]+1):
+        simulate(next, cnt+1)
+
+simulate(0, 0)
+print(ans)
