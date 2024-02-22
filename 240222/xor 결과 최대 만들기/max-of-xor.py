25d32b3b4767a07a1e608c80136233454bec0229
BMI-YOON <41634994+BMI-YOON@users.noreply.github.com>
2024-02-22 10:09:19 +0900

    Title: xor 결과 최대 만들기, Time: 144ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240222/xor 결과 최대 만들기/max-of-xor.py
@@ -0,0 +1,32 @@
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+
+selected = list()
+ans = 0
+
+def cal():
+    global selected 
+    
+    if len(selected) == 0:
+        return 0
+
+    output = selected[0]
+    for i in range(1, len(selected)):
+        output = output ^ selected[i]
+    return output
+
+def simulate(cur, cnt):
+    global n, m, arr, selected, ans 
+
+    if cur == n:
+        ans = max(ans, cal())
+        return 
+    
+    simulate(cur+1, cnt)
+
+    selected.append(arr[cur])
+    simulate(cur+1, cnt+1)
+    selected.pop()
+
+simulate(0, 0)
+print(ans)
