bede6f1fad325f40909ef96ce4a3e201e9543de3
BMI-YOON <41634994+BMI-YOON@users.noreply.github.com>
2024-02-22 12:02:28 +0900

    Title: 단순한 동전 챙기기, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240222/단순한 동전 챙기기/collect-coins-easy.py
@@ -0,0 +1,44 @@
+import sys
+
+N = int(input())
+grid = [
+    list(input())
+    for _ in range(N)
+]
+position = [(-1, -1)] * 11
+for i in range(N):
+    for j in range(N):
+        if grid[i][j] == 'S':
+            position[0] = (i, j)
+        elif grid[i][j] == 'E':
+            position[10] = (i, j)
+        elif grid[i][j] != '.':
+            position[int(grid[i][j])] = (i, j)
+
+ans = sys.maxsize
+
+def distance(previous, next):
+    global position 
+
+    x1, y1 = position[previous]
+    x2, y2 = position[next]
+    return abs(x1 - x2) + abs(y1 - y2)
+
+def simulate(previous, cur, cnt, cost):
+    global N, position, ans 
+    
+    if cnt == 3:
+        cost += distance(previous, 10)
+        ans = min(ans, cost)
+        return
+
+    if cur == 10:
+        return 
+    
+    simulate(previous, cur+1, cnt, cost)
+
+    cost += distance(previous, cur)
+    simulate(cur, cur+1, cnt+1, cost)
+
+simulate(0, 1, 0, 0)
+print(ans)
