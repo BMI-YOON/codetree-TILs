26c0ce1fec2ce199828f31c775d7fae841ed600a
BMI-YOON <41634994+BMI-YOON@users.noreply.github.com>
2024-02-23 11:25:58 +0900

    Title: n개의 점 중 m개 고르기, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240223/n개의 점 중 m개 고르기/choose-m-out-of-n-points.py
@@ -0,0 +1,44 @@
+import sys
+
+n, m = tuple(map(int, input().split()))
+arr = list()
+for _ in range(n):
+    arr.append(tuple(map(int, input().split())))
+
+def dist(dot1, dot2):
+    x1, y1 = arr[dot1]
+    x2, y2 = arr[dot2]
+    return (x1 - x2) ** 2 + (y1 - y2) ** 2
+
+grid = [
+    [0] * n
+    for _ in range(n)
+]
+for i in range(n):
+    for j in range(n):
+        grid[i][j] = dist(i, j)
+
+selected = list()
+ans = sys.maxsize
+
+def simulate(cur):
+    global n, m, arr, grid, selected, ans
+
+    if len(selected) == m:
+        min_val = sys.maxsize 
+        for i in range(len(selected)):
+            for j in range(i+1, len(selected)):
+                min_val = min(min_val, grid[i][j])
+        ans = min(ans, min_val)
+        return 
+    
+    if cur == n:
+        return 
+    
+    simulate(cur+1)
+
+    selected.append(cur)
+    simulate(cur+1)
+
+simulate(0)
+print(ans)
