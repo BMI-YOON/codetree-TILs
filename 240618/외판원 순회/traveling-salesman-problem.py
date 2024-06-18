3286737e3c30d4113ef16acd692e304896ba01ba
BMI-YOON <41634994+BMI-YOON@users.noreply.github.com>
2024-06-18 18:57:24 +0900

    Title: 외판원 순회, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240618/외판원 순회/traveling-salesman-problem.py
+++ 240618/외판원 순회/traveling-salesman-problem.py
@@ -8,20 +8,21 @@ grid = [
 answer = sys.maxsize
 arr = [0]
 visited = [False] * n
+visited[0] = True
 
 def f(i):
     global answer 
 
     if i == n:
         sum_val = 0
-        for idx in range(1, n+1):
+        for idx in range(1, n):
             sum_val += grid[arr[idx-1]][arr[idx]]
-        sum_val += grid[0][arr[-1]]
+        sum_val += grid[arr[-1]][0]
         answer = min(answer, sum_val)
         return
     
     for num in range(n):
-        if visited[num] == True or grid[num][arr[-1]] == 0:
+        if visited[num] == True or grid[arr[-1]][num] == 0:
             continue
         arr.append(num)
         visited[num] = True 
@@ -29,5 +30,5 @@ def f(i):
         arr.pop()
         visited[num] = False 
 
-f(0)
+f(1)
 print(answer)
